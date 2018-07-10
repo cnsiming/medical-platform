778c0edef22cbd1fcded9aa6179038c3d75af40e
lentoo <729533020@qq.com>
2018-07-10 14:07:29 +0800

    （1）完成首页热么分类列表 （2）完成搜索页面 （3）router跳转添加进度条 （4）优化代码 （5）优化rem布局

--- static/js/resize.js
+++ static/js/resize.js
@@ -1,7 +1,7 @@
 (function htmlResize () {
-  var devierWidth = document.documentElement.clientWidth
+  var devierWidth = document.documentElement.clientWidth > 750 ? 750 : document.documentElement.clientWidth
   var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
-  document.documentElement.style.fontSize = devierWidth / 3.75 + 'px'
+  document.documentElement.style.fontSize = devierWidth / (7.5 / (devicePixelRatio >= 2 ? 2 : 1)) + 'px'
   addEventListener(resizeEvt, htmlResize)
   // document.addEventListener('DOMContentLoaded', resizeEvt, false)
 })()
