314337548eead22407dab8b3ab3162ca36b66a30
lentoo <729533020@qq.com>
2018-08-14 14:36:45 +0800

    使用better-scroll 完成分类列表滚动效果

--- src/modules/home/index/pages/home.vue
+++ src/modules/home/index/pages/home.vue
@@ -79,32 +79,34 @@
       </section>
 
       <!-- 热门分类 -->
-      <section v-category ref="category" class="category">
+      <section ref="category" class="category">
         <div class="category_title">
           <h4>热门分类</h4>
           <p>优质商品，实力厂家，满满优惠惊喜等着您</p>
         </div>
-        <div class="category_list">
-          <div class="category_ul_list">
+        <div class="category_list" style="height: 100%;">
+          <div class="category_ul_list" style="height: 100%;overflow: hidden;">
             <ul>
-              <li @click="categoryChange(index)" v-for="(cate,index) in cates" :key="index" :class="index==currCate?'active':''">
+              <li @click="categoryChange(index,$event)" v-for="(cate,index) in cates" :key="index" :class="index==currCate?'active':''">
                 <span>{{cate.parent_name}}</span>
               </li>
             </ul>
           </div>
-          <div ref="contentList" class="category_content_list">
-            <div v-for="(cate,index) in cates" :key="index" class="category-item">
-              <div class="category-item-title">
-                <span class="vm"></span>
-                <p>{{cate.parent_name}}</p>
-                <span class="vm"></span>
-              </div>
-              <div class="clear category-item-imgs">
-                <div v-for="(imgs,i) in cate.child" :key="i" class="category-img">
-                  <a :href="'/product/categorylist?pid='+imgs.parent_category_id+'&cid='+imgs.category_id">
-                    <img :src="imgs.image_list" alt="">
-                    <p>{{imgs.category_name}}</p>
-                  </a>
+          <div style="height: calc(100vh - 1.6rem);overflow: hidden;" ref="contentList" class="category_content_list">
+            <div class="content_warpper">
+              <div v-for="(cate,index) in cates" :key="index" class="category-item">
+                <div class="category-item-title">
+                  <span class="vm"></span>
+                  <p>{{cate.parent_name}}</p>
+                  <span class="vm"></span>
+                </div>
+                <div class="clear category-item-imgs">
+                  <div v-for="(imgs,i) in cate.child" :key="i" class="category-img">
+                    <a :href="'/product/categorylist?pid='+imgs.parent_category_id+'&cid='+imgs.category_id">
+                      <img :src="imgs.image_list" alt="">
+                      <p>{{imgs.category_name}}</p>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -119,6 +121,7 @@
 import Vue from 'vue'
 import { mapActions } from 'vuex'
 import { Lazyload, Tabbar, TabbarItem, Swipe, SwipeItem } from 'vant'
+import BScroll from 'better-scroll'
 import twMenu from '@tw/tw-menu'
 import twMenuItem from '@tw/tw-menu-item'
 import Special from '../components/special'
@@ -127,8 +130,6 @@ import Activity from '../components/activity'
 import Gifs from '../components/gifs'
 
 import { getSp, getSlide, getCategory, activityindexshow } from '@/fetch/index'
-
-import easeout from '@/modules/common/js/animation'
 
 Vue.use(Lazyload)
   .use(Tabbar)
@@ -147,6 +148,8 @@ export default {
   },
   data () {
     return {
+      scrollRight: null,
+      scrollLeft: null,
       headerActive: false,
       imgs: [], // 上方轮播图
       adBannerImgs: [], // 广告轮播图
@@ -234,7 +237,8 @@ export default {
       scrollPosition: 0,
 
       // 促销活动专区
-      activityItems: []
+      activityItems: [],
+      heights: [0]
     }
   },
 
@@ -244,6 +248,9 @@ export default {
     })
     getCategory().then(res => {
       this.cates = res.data.data.cates
+      this.$nextTick(() => {
+        this._initScroll()
+      })
     })
     getSp().then(res => {
       this.total_activitys = res.data.data.total_activitys
@@ -252,17 +259,13 @@ export default {
     activityindexshow().then(res => {
       this.activityItems = res.data.data.activity_show
     })
-    try {
-      // 判断是否登录
-      // let isLogin = document.querySelectorAll('#foot .is_login')[0].value
-    } catch (error) {
-      console.log('err', error.message)
-    }
   },
   /**
    * 页面挂载完成后执行
    */
-  mounted () {},
+  mounted () {
+
+  },
 
   activated () {
     // 是否显示头部
@@ -318,86 +321,6 @@ export default {
             }
           })
       }
-    },
-    category: {
-      inserted: function (el, c, vnode) {
-        let categoryUl = null
-        let categoryLi = null
-        let categorys = null
-        let headerHeight = null
-        let categoryTitleHeight = null
-        let category = null
-        let categoryUlList = null
-        /** 处理分类 */
-        document
-          .getElementsByClassName('main')[0]
-          .addEventListener('scroll', function () {
-            headerHeight =
-              headerHeight ||
-              document.querySelectorAll('.header')[0].offsetHeight
-            let top = this.scrollTop + headerHeight
-            let categoryOffsetTop = el.offsetTop
-            if (top >= categoryOffsetTop) {
-              el.classList.add('on')
-            } else {
-              if (el.classList.contains('on')) {
-                el.classList.remove('on')
-                return
-              }
-            }
-
-            if (vnode.context.scroll) {
-              return
-            }
-            // 当滚动到热门分类时 才进行计算
-            category = category || document.querySelector('.category')
-            if (top < category.offsetTop) return
-
-            // 计算分类列表的偏移量
-            categoryTitleHeight =
-              categoryTitleHeight ||
-              document.querySelectorAll('.category_title')[0].offsetHeight
-            categorys =
-              categorys || document.querySelectorAll('.category-item')
-            top = top + categoryTitleHeight
-            let cateIndex = 0
-            for (let index = 0; index < categorys.length; index++) {
-              const item = categorys[index]
-              if (item.offsetTop - top <= 0) {
-                cateIndex = index
-              }
-            }
-            categoryUl =
-              categoryUl || document.querySelector('.category_list ul')
-
-            categoryLi =
-              categoryLi || document.querySelectorAll('.category_list li')
-
-            categoryUlList =
-              categoryUlList || document.querySelector('.category_ul_list')
-
-            // 当前的 li 距离父容器的距离 加上自己的高度 大于 ul list 的高度后，进行滚动
-            let liOffsetTop =
-              categoryLi[cateIndex].offsetTop +
-              categoryLi[cateIndex].offsetHeight +
-              categoryUlList.scrollTop
-
-            if (liOffsetTop > categoryUlList.offsetHeight) {
-              // 需要滚动的距离 = 当前的 li 距离父容器的距离 加上自己的高度  - 父容器的高度
-              categoryUlList.scrollTop =
-                liOffsetTop - categoryUlList.offsetHeight
-            } else {
-              // 向上滚动的时候
-              if (
-                categoryLi[cateIndex].offsetTop - categoryUlList.scrollTop <
-                0
-              ) {
-                categoryUlList.scrollTop = categoryLi[cateIndex].offsetTop
-              }
-            }
-            vnode.context.currCate = cateIndex
-          })
-      }
     }
   },
 
@@ -419,39 +342,48 @@ export default {
     /**
      * 改变当前分类
      */
-    categoryChange (index) {
-      this.currCate = index
-      this.scroll = true
-      let main = document.querySelectorAll('.home .main')[0]
-      let scrollTop = main.scrollTop
-      let offsetTop = this.$refs.category.offsetTop - 50
-      // 判断是否要将页面滚动到分类部分
-      if (scrollTop < offsetTop) {
-        main.scrollTop = offsetTop
+    categoryChange (index, event) {
+      if (!event._constructed) {
+        return
       }
-
-      // 计算当前分类距离页面的偏移量
-      let scrollHeight =
-        document.querySelectorAll('.category-item')[index].offsetTop -
-        document.querySelectorAll('.category_title')[0].offsetHeight -
-        document.querySelectorAll('.header')[0].offsetHeight
-      // main.scrollTop = scrollHeight
-      easeout(
-        main.scrollTop,
-        scrollHeight,
-        6,
-        function (value) {
-          main.scrollTop = value
-        },
-        function () {
-          clearTimeout(this.scrollTimer)
-          this.scrollTimer = setTimeout(() => {
-            this.scroll = false
-          }, 200)
-        }.bind(this)
-      )
+      this.currCate = index
+      this.scrollRight.scrollTo(0, -this.heights[index], 300)
+      this.scrollLeft.scrollToElement(document.querySelector('.category_ul_list li:nth-child(' + (index + 1) + ')'), null, true, true)
+      this.scrollLeft.refresh()
     },
 
+    _initScroll () {
+      this.scrollLeft = new BScroll('.category_ul_list', {
+        scrollY: true,
+        click: true
+      })
+      this.scrollRight = new BScroll('.category_content_list', {
+        scrollY: true,
+        probeType: 3
+      })
+
+      this.scrollRight.on('scroll', (ops) => {
+        this.heights.forEach((height, index) => {
+          let y = Math.abs(Math.round(ops.y))
+          let height2 = this.heights[index + 1]
+          if (height2 && (height <= y && y < height2)) {
+            this.currCate = index
+            this.scrollLeft.scrollToElement(document.querySelector('.category_ul_list li:nth-child(' + (index + 1) + ')'), null, true, true)
+            this.scrollLeft.refresh()
+          }
+        })
+      })
+
+      let items = document.querySelectorAll('.category_content_list .category-item')
+      items.forEach((el, index) => {
+        if (index > 0) {
+          let preItem = this.heights[index]
+          this.heights.push(preItem + el.offsetHeight)
+        } else {
+          this.heights.push(el.offsetHeight)
+        }
+      })
+    },
     /**
      * 点击搜索框
      */
